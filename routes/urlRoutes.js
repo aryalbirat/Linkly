31d9bd16ec0fe7073869e2bba6ec35a82f06b2b2
Birat <itsbirataryal+github@gmail.com>
2025-05-13 19:02:15 +0545

    some changes

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -8,7 +8,8 @@ router.post('/shorten', urlController.shortenUrl);
 // Route to get all URLs
 router.get('/all', urlController.getAllUrls);
 
-// Route to redirect to original URL - This will be accessed via the root path
+// Route to redirect to original URL 
+// This will be accessed via the root path
 // and is handled in index.js
 router.get('/:urlId', urlController.redirectToUrl);
 
